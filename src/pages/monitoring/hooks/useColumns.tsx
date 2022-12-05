d430d3ce8565a469807032bc8ba5cb6ba0a4f2c3
Susetyo Pambudi <susetyamulia@gmail.com>
2022-12-05 23:39:12 +0700

    feat: add color on row monitoring

--- src/pages/monitoring/hooks/useColumns.tsx
+++ src/pages/monitoring/hooks/useColumns.tsx
@@ -1,64 +1,63 @@
-import type { ColumnsType } from 'antd/es/table';
-import moment from 'moment';
-import {DataType} from '../store';
-import userLoginStore from '@/pages/login/store'
+import type { ColumnsType } from "antd/es/table";
+import moment from "moment";
+import { DataType } from "../store";
+import userLoginStore from "@/pages/login/store";
 
 function useColumns() {
-  const {username} = userLoginStore((state)=>(state))
+  const { username } = userLoginStore((state) => state);
   const columns: ColumnsType<DataType> = [
     {
-      title: 'ID Barang',
-      dataIndex: 'nomor_material',
-      key: 'nomor_material',
+      title: "ID Barang",
+      dataIndex: "nomor_material",
+      key: "nomor_material",
     },
     {
-      title: 'Nama Barang',
-      dataIndex: 'nama_material',
-      key: 'nama_material',
+      title: "Nama Barang",
+      dataIndex: "nama_material",
+      key: "nama_material",
     },
     {
-      title: 'Qty',
-      dataIndex: 'qty',
-      key: 'qty',
+      title: "Qty",
+      dataIndex: "qty",
+      key: "qty",
     },
     {
-      title: 'Posisi',
-      dataIndex: 'posisi',
-      key: 'posisi',
+      title: "Posisi",
+      dataIndex: "posisi",
+      key: "posisi",
     },
     {
-      title: 'Date',
-      dataIndex:'date',
-      key:'date',
-      render:(_, record) => {
-        const formatDate = moment(new Date(record.date)).format("YYYY-MM-DD")
-        return (<div>{formatDate}</div>)
-    
-      }
+      title: "Date",
+      dataIndex: "date",
+      key: "date",
+      render: (_, record) => {
+        const formatDate = moment(new Date(record.date)).format("YYYY-MM-DD");
+        return <div>{formatDate}</div>;
+      },
     },
     {
-      title: 'Type',
-      dataIndex: 'type',
-      key: 'type',
+      title: "Type",
+      dataIndex: "type",
+      key: "type",
     },
     {
-      title: 'Keterangan',
-      dataIndex: 'note',
-      key: 'note',
+      title: "Keterangan",
+      dataIndex: "note",
+      key: "note",
     },
     {
-      title: 'User',
-      dataIndex: 'id',
-      key: 'id',
-      fixed: 'right',
+      title: "User",
+      dataIndex: "id",
+      key: "id",
+      fixed: "right",
       width: 150,
-      render:(_,record) => (<div>{record.username}</div>)
+      render: (_, record) => <div>{record.username}</div>,
     },
   ];
 
   return {
-    columns
-  }
+    columns,
+  };
 }
 
-export default useColumns
+export default useColumns;
--- src/pages/monitoring/index.tsx
+++ src/pages/monitoring/index.tsx
@@ -1,13 +1,18 @@
-import useMonitoring from "./hooks/useMonitoring"
-import useColumns from "./hooks/useColumns"
-import {Table, Input, Button} from 'antd'
-import {SearchOutlined, FilterOutlined, FilePdfOutlined } from '@ant-design/icons';
-import FilterSide from './components/FilterSide'
+import useMonitoring from "./hooks/useMonitoring";
+import useColumns from "./hooks/useColumns";
+import { Table, Input, Button } from "antd";
+import {
+  SearchOutlined,
+  FilterOutlined,
+  FilePdfOutlined,
+} from "@ant-design/icons";
+import FilterSide from "./components/FilterSide";
 import useMonitoringStore from "./store";
+import "./style.css";
 
 function index() {
   const {
-    onChangePagination,     
+    onChangePagination,
     page,
     form,
     onChangeSearchInput,
@@ -18,18 +23,18 @@ function index() {
     onChangeRangePicker,
     onSelectType,
     onSubmitFilterSide,
-    onGeneratePdf
-  } = useMonitoring()
+    onGeneratePdf,
+  } = useMonitoring();
 
-  const {dataTable} = useMonitoringStore((state)=>state)
-  const {columns} = useColumns()
+  const { dataTable } = useMonitoringStore((state) => state);
+  const { columns } = useColumns();
 
   return (
     <div className="bg-white p-5">
       <h1 className="mb-2 text-xl">MONITORING</h1>
       <div className="w-full flex justify-between flex-wrap">
         <div className="flex flex-wrap gap-4">
-          <Input 
+          <Input
             size="large"
             placeholder="search"
             prefix={<SearchOutlined />}
@@ -46,36 +51,44 @@ function index() {
           </Button>
         </div>
         <Button
-            className="flex items-center"
-            size="large"
-            icon={<FilePdfOutlined />}
-            onClick={onGeneratePdf}
-          >
-            Generate PDF
-          </Button>
+          className="flex items-center"
+          size="large"
+          icon={<FilePdfOutlined />}
+          onClick={onGeneratePdf}
+        >
+          Generate PDF
+        </Button>
       </div>
-      <Table 
-        columns={columns} 
+      <Table
+        className="table-monitoring-majoin"
+        rowClassName={(record, index) => {
+          if (record?.type === "incoming") return "bg-red-400 text-white";
+          return "";
+        }}
+        columns={columns}
         dataSource={dataTable.rows}
         loading={queryMonitoring.isLoading}
         scroll={{ x: 1500 }}
         pagination={{
-          current:page, 
-          pageSize:10,
-          total:dataTable.totalRows,
-          onChange:(page,pageSize) => {onChangePagination({page,pageSize})}
-        }} />
-        <FilterSide
-          form={form}
-          title="Filter"
-          show={open}
-          onChangeRangePicker={onChangeRangePicker}
-          onSelect={onSelectType}
-          onClose={onClose}
-          onSubmitFilterSide={onSubmitFilterSide}
-        />
+          current: page,
+          pageSize: 10,
+          total: dataTable.totalRows,
+          onChange: (page, pageSize) => {
+            onChangePagination({ page, pageSize });
+          },
+        }}
+      />
+      <FilterSide
+        form={form}
+        title="Filter"
+        show={open}
+        onChangeRangePicker={onChangeRangePicker}
+        onSelect={onSelectType}
+        onClose={onClose}
+        onSubmitFilterSide={onSubmitFilterSide}
+      />
     </div>
-  )
+  );
 }
 
-export default index
+export default index;
